54f66c83156f7bd83deafd6ae73e5a9340675972
David VanScott <david.vanscott@gmail.com>
2013-11-16 11:36:00 -0500

    Update the icons

--- app/config/icons.php
+++ app/config/icons.php
@@ -10,16 +10,23 @@ return array(
 
 	'edit' => '<span class="icn" data-icon="3"></span>',
 
+	'facebook' => '<span class="icn" data-icon="c"></span>',
+
 	'gear' => '<span class="icn" data-icon="2"></span>',
 	'golf' => '<span class="icn" data-icon="g"></span>',
 
 	'remove' => '<span class="icn" data-icon="-"></span>',
 	'ruler' => '<span class="icn" data-icon="f"></span>',
 
+	'schedule' => '<span class="icn" data-icon="o"></span>',
 	'school' => '<span class="icn" data-icon="s"></span>',
 	'star' => '<span class="icn" data-icon="1"></span>',
 
+	'twitter' => '<span class="icn" data-icon="w"></span>',
+
 	'user' => '<span class="icn" data-icon="l"></span>',
 	'users' => '<span class="icn" data-icon="t"></span>',
+
+	'warning' => '<span class="icn" data-icon="!"></span>',
 
 );
