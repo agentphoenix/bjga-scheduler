ef21d97c003c705af255df9c33a5a76a79b11f77
David VanScott <david.vanscott@gmail.com>
2013-11-16 11:36:57 -0500

    New layout for the staff edit page

--- app/Scheduler/views/pages/admin/staff/edit.blade.php
+++ app/Scheduler/views/pages/admin/staff/edit.blade.php
@@ -24,96 +24,109 @@
 		</div>
 	@endif
 
-	{{ Form::model($staff, array('route' => array('admin.staff.update', $staff->id), 'method' => 'put')) }}
-		<div class="row">
-			<div class="col-lg-6">
-				<fieldset>
-					<legend>My Info</legend>
+	<ul class="nav nav-tabs">
+		<li class="active"><a href="#myInfo" data-toggle="tab">My Info</a></li>
+		<li><a href="#scheduleRegular" data-toggle="tab">Regular Schedule</a></li>
+		<li><a href="#scheduleExceptions" data-toggle="tab">Schedule Exceptions</a></li>
+	</ul>
 
-					@if ($_currentUser->access() > 1)
-						<div class="row">
-							<div class="col-lg-8">
-								<div class="form-group">
-									<label class="label-control">Title</label>
-									{{ Form::text('title', null, array('class' => 'form-control')) }}
-								</div>
-							</div>
-						</div>
-
-						<div class="row">
-							<div class="col-lg-4">
-								<div class="form-group{{ ($errors->has('access')) ? ' has-error' : '' }}">
-									<label class="label-control">Access Level</label>
-									{{ Form::text('access', null, array('class' => 'form-control input-with-feedback')) }}
-									{{ $errors->first('access', '<p class="help-block">:message</p>') }}
-								</div>
-							</div>
-						</div>
-
-						<div class="row">
-							<div class="col-lg-4">
-								<div class="form-group">
-									<label class="label-control">Available for Instruction</label>
-									<div>
-										<label class="radio-inline">{{ Form::radio('instruction', (int) true) }} Yes</label>
-										<label class="radio-inline">{{ Form::radio('instruction', (int) false) }} No</label>
+	<div class="tab-content">
+		<div id="myInfo" class="tab-pane active">
+			{{ Form::model($staff, array('route' => array('admin.staff.update', $staff->id), 'method' => 'put')) }}
+				<div class="row">
+					<div class="col-lg-6">
+						@if ($_currentUser->access() > 1)
+							<div class="row">
+								<div class="col-lg-8">
+									<div class="form-group">
+										<label class="label-control">Title</label>
+										{{ Form::text('title', null, array('class' => 'form-control')) }}
 									</div>
 								</div>
 							</div>
-						</div>
-					@endif
 
-					<div class="row">
-						<div class="col-lg-12">
-							<div class="form-group">
-								<label class="label-control">Bio</label>
-								{{ Form::textarea('bio', null, array('class' => 'form-control', 'rows' => 8)) }}
+							<div class="row">
+								<div class="col-lg-4">
+									<div class="form-group{{ ($errors->has('access')) ? ' has-error' : '' }}">
+										<label class="label-control">Access Level</label>
+										{{ Form::select('access', array('1' => 'Level 1', '2' => 'Level 2'), null, array('class' => 'form-control input-with-feedback')) }}
+										{{ $errors->first('access', '<p class="help-block">:message</p>') }}
+									</div>
+								</div>
 							</div>
-						</div>
-					</div>
-				</fieldset>
-			</div>
 
-			<div class="col-lg-6">
-				<fieldset>
-					<legend>Regular Schedule</legend>
+							<div class="row">
+								<div class="col-lg-6">
+									<div class="form-group">
+										<label class="label-control">Available for Instruction</label>
+										<div>
+											<label class="radio-inline">{{ Form::radio('instruction', (int) true) }} Yes</label>
+											<label class="radio-inline">{{ Form::radio('instruction', (int) false) }} No</label>
+										</div>
+									</div>
+								</div>
+							</div>
+						@endif
 
-					<p>Enter your schedule for each day of the week. If you have no availability on a day, simply leave it blank. Times should be in a 24-hour format (e.g. 18:00).</p>
-
-					<div class="visible-lg">
-						<p><a href="#" class="btn btn-default">Enter Schedule Exception</a></p>
-					</div>
-					<div class="hidden-lg">
-						<p><a href="#" class="btn btn-lg btn-block btn-default">Enter Schedule Exception</a></p>
-					</div>
-
-					<hr>
-
-					@for ($d = 0; $d <=6; $d++)
 						<div class="row">
-							<div class="col-sm-5 col-lg-3"><strong>{{ $days[$d] }}</strong></div>
-							<div class="col-sm-7 col-lg-9">
+							<div class="col-lg-12">
 								<div class="form-group">
-									{{ Form::text('schedule['.$d.']', $schedule->filter(function($s) use($d){ return $s->day == $d; })->first()->availability, array('class' => 'form-control')) }}
+									<label class="label-control">Bio</label>
+									{{ Form::textarea('bio', null, array('class' => 'form-control', 'rows' => 8)) }}
 								</div>
 							</div>
 						</div>
-					@endfor
-				</fieldset>
-			</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="visible-lg">
+							{{ Form::button('Submit', array('type' => 'submit', 'class' => 'btn btn-primary')) }}
+						</div>
+						<div class="hidden-lg">
+							{{ Form::button('Submit', array('type' => 'submit', 'class' => 'btn btn-lg btn-block btn-primary')) }}
+						</div>
+					</div>
+				</div>
+			{{ Form::close() }}
 		</div>
 
-		<div class="row">
-			<div class="col-lg-12">
-				<div class="visible-lg">
-					{{ Form::button('Submit', array('type' => 'submit', 'class' => 'btn btn-primary')) }}
+		<div id="scheduleRegular" class="tab-pane">
+			{{ Form::model($staff, array('route' => array('admin.staff.update', $staff->id), 'method' => 'put')) }}
+				
+				<p>Enter your schedule for each day of the week. If you have no availability on a day, simply leave it blank. Times should be in a 24-hour format (e.g. 18:00).</p>
+
+				<div class="row">
+					<div class="col-lg-6">
+						@for ($d = 0; $d <=6; $d++)
+							<div class="row">
+								<div class="col-sm-5 col-lg-3"><strong>{{ $days[$d] }}</strong></div>
+								<div class="col-sm-7 col-lg-9">
+									<div class="form-group">
+										{{ Form::text('schedule['.$d.']', $schedule->filter(function($s) use($d){ return $s->day == $d; })->first()->availability, array('class' => 'form-control')) }}
+									</div>
+								</div>
+							</div>
+						@endfor
+					</div>
 				</div>
-				<div class="hidden-lg">
-					{{ Form::button('Submit', array('type' => 'submit', 'class' => 'btn btn-lg btn-block btn-primary')) }}
+
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="visible-lg">
+							{{ Form::button('Submit', array('type' => 'submit', 'class' => 'btn btn-primary')) }}
+						</div>
+						<div class="hidden-lg">
+							{{ Form::button('Submit', array('type' => 'submit', 'class' => 'btn btn-lg btn-block btn-primary')) }}
+						</div>
+					</div>
 				</div>
-			</div>
+			{{ Form::close() }}
 		</div>
-	{{ Form::close() }}
+
+		<div id="scheduleExceptions" class="tab-pane"></div>
+	</div>
 
 	{{ modal(array('id' => 'staffExceptions', 'header' => "Set Schedule Exception")) }}
 @endsection
