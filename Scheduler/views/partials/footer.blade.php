301d3aa22e5c20afa2285b3227068d50dad0aa1e
David VanScott <david.vanscott@gmail.com>
2014-01-07 17:57:39 -0500

    More icon and footer tweaks

--- Scheduler/views/partials/footer.blade.php
+++ Scheduler/views/partials/footer.blade.php
@@ -1,25 +1,24 @@
 <footer class="visible-lg">
 	<div class="row">
-		<div class="col-lg-8">
+		<div class="col-lg-12">
 			<div class="inner">
 				<h1>Brian Jacobs Golf Academy at Mill Creek Golf Club</h1>
 				
 				<p>A 23-acre practice center at Mill Creek Golf Club accommodates all levels of golfers serious about their game. The bent grass driving range has over 400 yards of hitting area with six target greens and the short game practice area includes three separate greens for pitching/chipping, putting and bunker play.</p>
+
+				<div class="btn-toolbar pull-right">
+					<div class="btn-group">
+						<a href="http://maps.google.com/?q=128 Cedars Ave Rochester, NY 14428" class="btn btn-sm btn-default icn-size-16" target="_blank">{{ $_icons['map'] }}</a>
+					</div>
+					<div class="btn-group">
+						<a href="https://www.facebook.com/brianjacobsgolf" target="_blank" class="btn btn-sm btn-default facebook icn-size-16">{{ $_icons['facebook'] }}</a>
+						<a href="http://twitter.com/BrianJacobsgolf" target="_blank" class="btn btn-sm btn-default twitter icn-size-16">{{ $_icons['twitter'] }}</a>
+					</div>
+				</div>
 				
 				<p>128 Cedars Avenue<br>
 				Rochester, NY 14428<br>
 				(585) 889-4110</p>
-
-				<p><a href="http://maps.google.com/?q=128 Cedars Ave Rochester, NY 14428" class="btn btn-sm btn-default" target="_blank"><span class="icn-size-16">{{ $_icons['map'] }}</span></a></p>
-			</div>
-		</div>
-		<div class="col-lg-4">
-			<div class="inner">
-				<div class="pull-right">
-					<a href="https://www.facebook.com/brianjacobsgolf" target="_blank" class="icn-size-24 facebook"><span class="icn" data-icon="c"></span></a>
-					&nbsp;&nbsp;
-					<a href="http://twitter.com/BrianJacobsgolf" target="_blank" class="icn-size-24 twitter"><span class="icn" data-icon="w"></span></a>
-				</div>
 			</div>
 		</div>
 	</div>
