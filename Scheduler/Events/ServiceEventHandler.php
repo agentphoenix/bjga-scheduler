e09a6f2c6b92ab6d0461b09f320a97eb8d778ce8
David VanScott <david.vanscott@gmail.com>
2014-01-29 19:40:44 -0500

    Work on the service event handler

--- Scheduler/Events/ServiceEventHandler.php
+++ Scheduler/Events/ServiceEventHandler.php
@@ -1,11 +1,65 @@
 <?php namespace Scheduler\Events;
 
+use Mail, Queue;
+
 class ServiceEventHandler {
 
-	public function onCreated($service, $input){}
+	public function onCreated($service, $input)
+	{
+		if ($service->isProgram())
+		{
+			// Update the calendar
+			Queue::push('Scheduler\Services\CalendarService', array('model' => $service));
+		}
+	}
 
-	public function onDeleted($service){}
+	public function onDeleted($service)
+	{
+		if ($service->isProgram())
+		{
+			// Update the calendar
+			Queue::push('Scheduler\Services\CalendarService', array('model' => $service));
 
-	public function onUpdated($service, $input){}
+			// Set the data for the email
+			$data = array(
+				'service' => $service
+			);
+
+			// Set the recipients
+			$recipients = implode(',', $service->attendees()->toSimpleArray('id', 'email'));
+
+			// Email the attendees
+			Mail::queue('emails.services.deleted', $data, function($message) use ($recipients, $service)
+			{
+				$message->to($recipients)
+					->subject("[Brian Jacobs Golf] {$service->name} Has Been Cancelled");
+			});
+		}
+	}
+
+	public function onUpdated($service, $input)
+	{
+		if ($service->isProgram())
+		{
+			// Update the calendar
+			Queue::push('Scheduler\Services\CalendarService', array('model' => $service));
+
+			// Set the data for the email
+			$data = array(
+				'service' => $service,
+				'schedule' => $service->serviceOccurrences,
+			);
+
+			// Set the recipients
+			$recipients = implode(',', $service->attendees()->toSimpleArray('id', 'email'));
+
+			// Email the attendees
+			Mail::queue('emails.services.updated', $data, function($message) use ($recipients, $service)
+			{
+				$message->to($recipients)
+					->subject("[Brian Jacobs Golf] {$service->name} Has Been Updated");
+			});
+		}
+	}
 
 }
---
+++ Scheduler/views/emails/services/updated.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h2>Service Update: {{ $service->name }}</h2>
+
+		<p>{{ $service->name }} has recently been updated. We've included updated information about the service below for your convenience. Please verify the information and ensure you've taken note of any changes to the schedule. If you have questions, please contact us.</p>
+
+		<p>Thank you, we look forward to serving you.</p>
+
+		<h3>Details</h3>
+
+		<p><strong>Description</strong> - {{ $service->description }}</p>
+		<p><strong>Price</strong> - ${{ $service->price }}</p>
+
+		<h3>Schedule</h3>
+
+		<ul>
+		@foreach ($schedule as $s)
+			<li>{{ $s->start->format('l F jS, Y') }}, {{ $s->start->format('g:ia') }} - {{ $s->end->format('g:ia') }}</li>
+		@endforeach
+		</ul>
+	</body>
+</html>
