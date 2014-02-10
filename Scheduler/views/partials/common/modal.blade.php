18d6bf99de121d9eb5ab31b756cb8159ba35f7b7
David VanScott <david.vanscott@gmail.com>
2014-02-09 22:39:22 -0500

    Fix some PHP 5.4 syntax

--- Scheduler/views/partials/common/modal.blade.php
+++ Scheduler/views/partials/common/modal.blade.php
@@ -1,7 +1,7 @@
 <div id="{{ $modalId }}" class="modal fade">
 	<div class="modal-dialog">
 		<div class="modal-content">
-			{{ partial('common/modal_content', ['modalHeader' => $modalHeader, 'modalBody' => $modalBody, 'modalFooter' => $modalFooter]) }}
+			{{ partial('common/modal_content', array('modalHeader' => $modalHeader, 'modalBody' => $modalBody, 'modalFooter' => $modalFooter)) }}
 		</div>
 	</div>
 </div>
