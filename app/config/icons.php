8e89e99563366c72f421da4ec0be58a64bd240d7
David VanScott <david.vanscott@gmail.com>
2014-02-10 18:36:42 -0500

    Font updates
    Closes #20

--- app/config/icons.php
+++ app/config/icons.php
@@ -7,6 +7,7 @@ return array(
 	'back' => '<span class="icn" data-icon="<"></span>',
 
 	'calendar' => '<span class="icn" data-icon="e"></span>',
+	'check' => '<span class="icn" data-icon="k"></span>',
 
 	'edit' => '<span class="icn" data-icon="3"></span>',
 	'email' => '<span class="icn" data-icon="v"></span>',
@@ -16,13 +17,18 @@ return array(
 	'gear' => '<span class="icn" data-icon="2"></span>',
 	'golf' => '<span class="icn" data-icon="g"></span>',
 
-	'logout' => '<span class="icn" data-icon="i"></span>',
+	'info' => '<span class="icn" data-icon="i"></span>',
+
+	'logout' => '<span class="icn" data-icon="x"></span>',
 
 	'map' => '<span class="icn" data-icon="m"></span>',
+	'money' => '<span class="icn" data-icon="n"></span>',
 
 	'phone' => '<span class="icn" data-icon="p"></span>',
 
+	'reject' => '<span class="icn" data-icon="b"></span>',
 	'remove' => '<span class="icn" data-icon="-"></span>',
+	'report' => '<span class="icn" data-icon="r"></span>',
 	'ruler' => '<span class="icn" data-icon="f"></span>',
 
 	'schedule' => '<span class="icn" data-icon="o"></span>',
