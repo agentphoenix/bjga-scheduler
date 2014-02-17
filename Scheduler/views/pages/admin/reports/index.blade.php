3fbc2f0505f7cd16bdfcee33d84c39348668704c
David VanScott <david.vanscott@gmail.com>
2014-02-17 15:56:45 -0500

    Minor style updates to report center

--- Scheduler/views/pages/admin/reports/index.blade.php
+++ Scheduler/views/pages/admin/reports/index.blade.php
@@ -10,12 +10,12 @@
 	<div class="row">
 		<div class="col-sm-6 col-md-6 col-lg-6">
 			<div class="text-center text-success icn-size-96">{{ $_icons['calendar'] }}</div>
-			<p><a href="{{ URL::route('admin.reports.monthly') }}" class="btn btn-lg btn-default btn-block">Monthly Report</a></p>
+			<p><a href="{{ URL::route('admin.reports.monthly') }}" class="btn btn-lg btn-primary btn-block">Monthly Report</a></p>
 		</div>
 
 		<div class="col-sm-6 col-md-6 col-lg-6">
-			<div class="text-center text-success icn-size-96">{{ $_icons['warning'] }}</div>
-			<p><a href="{{ URL::route('admin.reports.unpaid') }}" class="btn btn-lg btn-default btn-block">Unpaid Services</a></p>
+			<div class="text-center text-danger icn-size-96">{{ $_icons['warning'] }}</div>
+			<p><a href="{{ URL::route('admin.reports.unpaid') }}" class="btn btn-lg btn-danger btn-block">Unpaid Services</a></p>
 		</div>
 	</div>
 @stop
