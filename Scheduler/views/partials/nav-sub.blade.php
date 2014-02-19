b61b6a2ce25840b74214beec25e13602c8911b2d
David VanScott <david.vanscott@gmail.com>
2014-02-18 21:30:37 -0500

    Update the sub nav

--- Scheduler/views/partials/nav-sub.blade.php
+++ Scheduler/views/partials/nav-sub.blade.php
@@ -5,7 +5,7 @@
 		<li class="visible-xs visible-sm"><a href="http://brianjacobsgolf.com">Brian Jacobs Golf Home</a></li>
 		@if (Auth::check())
 			<li class="{{ (Request::is('/') ? 'active' : '') }}"><a href="{{ URL::route('home') }}"><span class="icn-size-16">{{ $_icons['schedule'] }}</span>My Schedule</a></li>
-			<li class="{{ (Request::is('event*') ? 'active' : '') }}"><a href="{{ URL::route('events') }}"><span class="icn-size-16">{{ $_icons['schedule'] }}</span>Upcoming Events</a></li>
+			<li class="{{ (Request::is('event*') ? 'active' : '') }}"><a href="{{ URL::route('events') }}"><span class="icn-size-16">{{ $_icons['calendar'] }}</span>Upcoming Events</a></li>
 			<li class="{{ (Request::is('admin/user/*') ? 'active' : '') }}"><a href="{{ URL::route('admin.user.edit', array($_currentUser->id)) }}"><span class="icn-size-16">{{ $_icons['user'] }}</span>My Account</a></li>
 			<li><a href="{{ URL::route('logout') }}"><span class="icn-size-16">{{ $_icons['logout'] }}</span>Log Out</a></li>
 		@else
@@ -19,7 +19,7 @@
 			<p>Admin</p>
 
 			<ul>
-				<li class="{{ (Request::is('admin/appointments*') ? 'active' : '') }}"><a href="#"><span class="icn-size-16">{{ $_icons['calendar'] }}</span>Appointments</a></li>
+				<li class="{{ (Request::is('admin/appointments*') ? 'active' : '') }}"><a href="#"><span class="icn-size-16">{{ $_icons['list'] }}</span>Appointments</a></li>
 				<li class="{{ (Request::is('admin/service*') ? 'active' : '') }}"><a href="{{ URL::route('admin.service.index') }}"><span class="icn-size-16">{{ $_icons['golf'] }}</span>Services</a></li>
 				<li class="{{ (Request::is('admin/user*') ? 'active' : '') }}"><a href="{{ URL::route('admin.user.index') }}"><span class="icn-size-16">{{ $_icons['users'] }}</span>Users</a></li>
 				<li class="{{ (Request::is('admin/staff*') ? 'active' : '') }}"><a href="{{ URL::route('admin.staff.index') }}"><span class="icn-size-16">{{ $_icons['school'] }}</span>Staff</a></li>
