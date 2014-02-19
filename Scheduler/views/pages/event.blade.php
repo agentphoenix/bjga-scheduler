6a2b814cbc3303734e0d5175babb7994e6e8de88
David VanScott <david.vanscott@gmail.com>
2014-02-18 19:12:21 -0500

    Working on the event page

--- Scheduler/views/pages/event.blade.php
+++ Scheduler/views/pages/event.blade.php
@@ -7,6 +7,44 @@
 @section('content')
 	<h1>{{ $event->name }}</h1>
 
+	<div class="visible-md visible-lg">
+		<div class="btn-toolbar">
+			<div class="btn-group">
+				<a href="{{ URL::route('events') }}" class="btn btn-sm btn-default icn-size-16">{{ $_icons['back'] }}</a>
+			</div>
+
+			@if ( ! $_currentUser->isAttending($appointment->id))
+				<div class="btn-group">
+					<a href="#" class="btn btn-primary icn-size-16 js-enroll" data-appointment="{{ $appointment->id }}">Enroll Now</a>
+				</div>
+			@endif
+
+			@if ($_currentUser->isAttending($appointment->id))
+				<div class="btn-group">
+					<a href="#" class="btn btn-danger icn-size-16 js-withdraw" data-appointment="{{ $_currentUser->getAppointment($appointment->id)->first()->id }}">Withdraw</a>
+				</div>
+			@endif
+		</div>
+	</div>
+	<div class="visible-xs visible-sm">
+		<div class="row">
+			<div class="col-xs-12 col-sm-3">
+				<p><a href="{{ URL::route('events') }}" class="btn btn-lg btn-block btn-default icn-size-16">{{ $_icons['back'] }}</a></p>
+			</div>
+			@if ( ! $_currentUser->isAttending($appointment->id))
+				<div class="col-xs-12 col-sm-3">
+					<p><a href="#" class="btn btn-lg btn-block btn-primary icn-size-16 js-enroll" data-appointment="{{ $appointment->id }}">Enroll Now</a></p>
+				</div>
+			@endif
+
+			@if ($_currentUser->isAttending($appointment->id))
+				<div class="col-xs-12 col-sm-3">
+					<p><a href="#" class="btn btn-lg btn-block btn-danger icn-size-16 js-withdraw" data-appointment="{{ $_currentUser->getAppointment($appointment->id)->first()->id }}">Withdraw</a></p>
+				</div>
+			@endif
+		</div>
+	</div>
+
 	<p>{{ $event->description }}</p>
 
 	@if ( ! empty($appointment->notes) and $_currentUser and $_currentUser->isStaff())
@@ -21,83 +59,37 @@
 	@endif
 
 	<div class="row">
-		<div class="col-sm-6 col-md-5 col-lg-5">
+		<div class="col-sm-8 col-md-8 col-lg-8">
 			<dl>
 				<dt>Date(s)</dt>
 				@foreach ($event->serviceOccurrences as $o)
-					<dd>{{ $o->start->format('l M jS, Y') }}, {{ $o->start->format('g:ia') }} - {{ $o->end->format('g:ia') }}</dd>
+					<dd>{{ $o->start->format(Config::get('bjga.dates.date')) }}</dd>
+					<dd>{{ $o->start->format(Config::get('bjga.dates.time')) }} - {{ $o->end->format(Config::get('bjga.dates.time')) }}</dd>
 				@endforeach
 
-				<dt>Location</dt>
-				<dd>{{ $appointment->location }}</dd>
-
 				<dt>Price</dt>
-				<dd>${{ $event->price }}</dd>
+				<dd>
+					@if ($event->price > 0)
+						${{ $event->price }}
+					@else
+						Free
+					@endif
+				</dd>
 
 				<dt>Instructor</dt>
 				<dd>{{ $event->staff->user->name }}</dd>
-			</dt>
+			</dl>
 		</div>
 
 		@if ($_currentUser)
-			<div class="col-sm-6 col-md-4 col-lg-4">
-				<dl>
-					@if ($_currentUser->isStaff())
-						<dt>Attendees ({{ $event->attendees()->count() }}/{{ $event->user_limit }})</dt>
-						@foreach ($appointment->userAppointments as $ua)
-							<dd>
-								@if ((bool) $ua->paid)
-									<span class="label label-success icn-size-16">{{ $_icons['check'] }}</span>
-								@else
-									<span class="label label-warning icn-size-16">{{ $_icons['warning'] }}</span>
-								@endif
+			<div class="col-sm-4 col-md-4 col-lg-4">
+				@if ( ! $_currentUser->isAttending($appointment->id))
+					<p><a href="#" class="btn btn-lg btn-block btn-primary js-enroll" data-appointment="{{ $appointment->id }}">Enroll Now</a></p>
+				@endif
 
-								{{ $ua->user->name }}
-							</dd>
-						@endforeach
-					@endif
-				</dt>
-			</div>
-
-			<div class="col-sm-12 col-md-3 col-lg-3">
-				<div class="visible-md visible-lg">
-					@if ($_currentUser != $event->staff->user)
-						@if ( ! $_currentUser->isAttending($appointment->id))
-							<a href="#" class="btn btn-block btn-primary js-enroll" data-appointment="{{ $appointment->id }}">Enroll Now</a>
-						@endif
-
-						@if ($_currentUser->isAttending($appointment->id))
-							<a href="#" class="btn btn-block btn-danger js-withdraw" data-appointment="{{ $_currentUser->getAppointment($appointment->id)->first()->id }}">Withdraw</a>
-						@endif
-					@endif
-
-					@if ($_currentUser->isStaff())
-						<a href="{{ URL::route('admin.service.edit', array($event->id)) }}" class="btn btn-block btn-default">Manage</a>
-
-						@if ($appointment->service->isLesson())
-							<a href="#" class="btn btn-block btn-primary">Mark as Paid</a>
-						@endif
-					@endif
-				</div>
-				<div class="visible-xs visible-sm">
-					@if ($_currentUser != $event->staff->user)
-						@if ( ! $_currentUser->isAttending($appointment->id))
-							<p><a href="#" class="btn btn-block btn-lg btn-primary js-enroll" data-appointment="{{ $appointment->id }}">Enroll Now</a></p>
-						@endif
-
-						@if ($_currentUser->isAttending($appointment->id))
-							<p><a href="#" class="btn btn-block btn-lg btn-danger js-withdraw" data-appointment="{{ $_currentUser->getAppointment($appointment->id)->first()->id }}">Withdraw</a></p>
-						@endif
-					@endif
-
-					@if ($_currentUser->isStaff())
-						<p><a href="{{ URL::route('admin.service.edit', array($event->id)) }}" class="btn btn-block btn-lg btn-default">Manage</a></p>
-
-						@if ($appointment->service->isLesson())
-							<a href="#" class="btn btn-block btn-lg btn-primary">Mark as Paid</a>
-						@endif
-					@endif
-				</div>
+				@if ($_currentUser->isAttending($appointment->id))
+					<a href="#" class="btn btn-lg btn-block btn-danger js-withdraw" data-appointment="{{ $_currentUser->getAppointment($appointment->id)->first()->id }}">Withdraw</a>
+				@endif
 			</div>
 		@endif
 	</div>
