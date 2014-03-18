9c3d315b6bcafaaba78e5d3e1a051f52b35e1e18
David VanScott <david.vanscott@gmail.com>
2014-03-18 08:55:34 -0400

    Fix issues with the attendees modal
    Fixes #53

--- Scheduler/views/pages/admin/appointments/ajax/attendees.blade.php
+++ Scheduler/views/pages/admin/appointments/ajax/attendees.blade.php
@@ -1,34 +1,48 @@
 @if ($attendees->count() > 0)
 	<div class="data-table data-table-striped data-table-bordered">
 	@foreach ($attendees as $a)
+		<?php
+
+		if ($a instanceof Scheduler\Models\Eloquent\UserModel)
+		{
+			$user = $a;
+			$apptBased = false;
+		}
+		else
+		{
+			$user = $a->user;
+			$apptBased = true;
+		}
+
+		?>
 		<div class="row">
 			<div class="col-xs-12 col-sm-6 col-md-8 col-lg-8">
-				<p><strong>{{ $a->user->name }}</strong></p>
+				<p><strong>{{ $user->name }}</strong></p>
 			</div>
 			<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
 				<div class="visible-md visible-lg">
 					<div class="btn-toolbar pull-right">
-						@if ((bool) $a->paid === false)
+						@if ($apptBased and (bool) $a->paid === false)
 							<div class="btn-group">
 								<a href="#" class="btn btn-sm btn-primary icn-size-16 js-markAsPaid" data-appt="{{ $a->id }}">{{ $_icons['check'] }}</a>
 							</div>
 						@endif
 						<div class="btn-group">
-							<a href="#" class="btn btn-sm btn-danger icn-size-16 js-removeAttendee" data-appt="{{ $a->appointment_id }}" data-user="{{ $a->user->id }}">{{ $_icons['reject'] }}</a>
+							<a href="#" class="btn btn-sm btn-danger icn-size-16 js-removeAttendee" data-appt="{{ $a->appointment_id }}" data-user="{{ $user->id }}">{{ $_icons['reject'] }}</a>
 						</div>
 					</div>
 				</div>
 				<div class="visible-xs visible-sm">
 					<div class="row">
 						<div class="col-xs-12 col-sm-6">
-							@if ((bool) $a->paid === false)
+							@if ($apptBased and (bool) $a->paid === false)
 								<p><a class="btn btn-lg btn-block btn-primary icn-size-16 js-markAsPaid" data-appt="{{ $a->id }}">{{ $_icons['check'] }}</a></p>
 							@else
 								<p class="hidden-xs visible-sm">&nbsp;</p>
 							@endif
 						</div>
 						<div class="col-xs-12 col-sm-6">
-							<p><a href="#" class="btn btn-lg btn-block btn-danger icn-size-16 js-removeAttendee" data-appt="{{ $a->appointment_id }}" data-user="{{ $a->user->id }}">{{ $_icons['reject'] }}</a></p>
+							<p><a href="#" class="btn btn-lg btn-block btn-danger icn-size-16 js-removeAttendee" data-appt="{{ $a->appointment_id }}" data-user="{{ $user->id }}">{{ $_icons['reject'] }}</a></p>
 						</div>
 					</div>
 				</div>
