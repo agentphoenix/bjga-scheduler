5e57744953f5e8d1342e0b3ea687a5b960ccd365
David VanScott <david.vanscott@gmail.com>
2014-03-21 22:08:58 -0400

    Add Dispatcher service provider

--- app/config/app.php
+++ app/config/app.php
@@ -107,6 +107,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 
+		'Indatus\Dispatcher\ServiceProvider',
 		'Scheduler\SchedulerServiceProvider',
 		'Scheduler\SchedulerRoutingServiceProvider',
 
