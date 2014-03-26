3be9d9d3db80b42d03c0ad0d2d88568f127270ef
David VanScott <david.vanscott@gmail.com>
2014-03-25 23:13:17 -0400

    Working on figuring out conflicts

--- app/routes.php
+++ app/routes.php
@@ -1,2 +1,11 @@
 <?php
 
+Route::get('conflicts', function()
+{
+	$date = Date::now();
+	$user = Auth::user();
+	$service = ServiceModel::find(2);
+
+	$a = new Scheduler\Services\AvailabilityService;
+	s($a->find($user, $date, $service));
+});
