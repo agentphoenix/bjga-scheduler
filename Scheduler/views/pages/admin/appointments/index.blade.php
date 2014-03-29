2d393ce0e4a16802929ff25174073bd7d9888d5d
David VanScott <david.vanscott@gmail.com>
2014-03-29 14:44:17 -0400

    Start working on recurring appointment management
    #45

--- Scheduler/views/pages/admin/appointments/index.blade.php
+++ Scheduler/views/pages/admin/appointments/index.blade.php
@@ -8,13 +8,17 @@
 	<h1>Manage Appointments</h1>
 
 	<div class="row">
-		<div class="col-sm-6 col-md-6 col-lg-6">
+		<div class="col-sm-4 col-md-4 col-lg-4">
 			<div class="text-center text-success icn-size-96">{{ $_icons['add'] }}</div>
 			<p><a href="{{ URL::route('admin.appointment.create') }}" class="btn btn-lg btn-primary btn-block">Add Appointment</a></p>
 		</div>
-		<div class="col-sm-6 col-md-6 col-lg-6">
+		<div class="col-sm-4 col-md-4 col-lg-4">
 			<div class="text-center text-success icn-size-96">{{ $_icons['users'] }}</div>
 			<p><a href="{{ URL::route('admin.appointment.user') }}" class="btn btn-lg btn-primary btn-block">Appointments by Student</a></p>
+		</div>
+		<div class="col-sm-4 col-md-4 col-lg-4">
+			<div class="text-center text-success icn-size-96">{{ $_icons['recur'] }}</div>
+			<p><a href="{{ URL::route('admin.appointment.user') }}" class="btn btn-lg btn-primary btn-block">Recurring Lessons</a></p>
 		</div>
 	</div>
 @stop
