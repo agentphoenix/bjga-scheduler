8ddc429695b9b0fd34e6352a040faacd5e7be4e8
David VanScott <david.vanscott@gmail.com>
2014-04-01 13:00:47 -0400

    Clean up the routes file

--- app/routes.php
+++ app/routes.php
@@ -1,10 +1,1 @@
 <?php
-
-Route::get('calendar', function()
-{
-	$sa = StaffAppointmentModel::find(1);
-
-	$q = Queue::push('Scheduler\Services\CalendarService', array('staff' => $sa->staff->id));
-
-	return 'Done!';
-});
