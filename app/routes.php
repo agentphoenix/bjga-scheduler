9809052574ef6b102dabb702d81bda6953ea5961
David VanScott <david.vanscott@gmail.com>
2014-03-31 22:53:09 -0400

    Clean up the routes file

--- app/routes.php
+++ app/routes.php
@@ -1,22 +1,5 @@
 <?php
 
-Route::get('conflicts', function()
-{
-	$date = Date::now();
-	$user = Auth::user();
-	$service = ServiceModel::find(2);
-
-	$a = new Scheduler\Services\AvailabilityService;
-	s($a->find($user, $date, $service));
-});
-
-Route::get('lists', function()
-{
-	$b = App::make('scheduler.bombbomb');
-
-	s($b->lists());
-});
-
 Route::get('calendar', function()
 {
 	$sa = StaffAppointmentModel::find(1);
