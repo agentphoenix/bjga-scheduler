93c308134b2899dbcdaf1c8e9dab09dfa8ce76fd
David VanScott <david.vanscott@gmail.com>
2014-03-31 20:06:19 -0400

    Clean up the reminder email

--- Scheduler/views/emails/auth/reminder.blade.php
+++ Scheduler/views/emails/auth/reminder.blade.php
@@ -4,10 +4,10 @@
 		<meta charset="utf-8">
 	</head>
 	<body>
-		<h2>Password Reset</h2>
+		<p>A password reset has been requested for your account. If you did not initiate this reset, please contact Brian Jacobs Golf immediately (admin@brianjacobsgolf.com).</p>
 
-		<div>
-			To reset your password, complete this form: {{ URL::to('password/reset', array($token)) }}.
-		</div>
+		<p>Using the link below, you can reset your password to something you can more easily remember.</p>
+
+		<p>{{ URL::to('password/reset', array($token)) }}</p>
 	</body>
 </html>
