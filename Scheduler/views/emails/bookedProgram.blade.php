6fbbd1cdc0b58c2a09603ad64aff35332fc5db44
David VanScott <david.vanscott@gmail.com>
2014-04-02 12:44:30 -0400

    Tweaked the enroll in program email

--- Scheduler/views/emails/bookedProgram.blade.php
+++ Scheduler/views/emails/bookedProgram.blade.php
@@ -4,7 +4,7 @@
 		<meta charset="utf-8">
 	</head>
 	<body>
-		<p>You have successfully enrolled in the {{ $service }}. The schedule for the program is included below for reference. Please try to arrive about 5 to 10 minutes before the scheduled start time(s) so we can begin on time. A reminder email will be sent the day of your appointment.</p>
+		<p>You have successfully enrolled in the {{ $service }} program. The schedule for the program is included below for reference. Please try to arrive about 10 minutes before the scheduled start time(s) so we can begin on time. A reminder email will be sent the day of your appointment(s).</p>
 
 		<p>Thank you, we look forward to serving you.</p>
 
@@ -12,7 +12,7 @@
 
 		<ul>
 		@foreach ($schedule as $s)
-			<li>{{ $s->start->format('l F jS, Y') }}, {{ $s->start->format('g:ia') }} - {{ $s->end->format('g:ia') }}</li>
+			<li>{{ $s['start']->format('l F jS, Y') }}, {{ $s['start']->format('g:ia') }} - {{ $s['end']->format('g:ia') }}</li>
 		@endforeach
 		</ul>
 	</body>
