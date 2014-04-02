0019f654d39dd045f3898a31439fd35c6b4399d8
David VanScott <david.vanscott@gmail.com>
2014-04-02 12:37:25 -0400

    Fix price on the book program page

--- Scheduler/Controllers/AjaxController.php
+++ Scheduler/Controllers/AjaxController.php
@@ -152,7 +152,7 @@ class AjaxController extends BaseController {
 		{
 			return View::make('pages.ajax.programServiceDetails')
 				->withDates($service->serviceOccurrences->sortBy(function($s) { return $s->start; }))
-				->withPrice($service->price);
+				->withPrice($service->present()->price);
 		}
 
 		return View::make('partials.common.alert')
