f4108db9142a984af4c32ae7edd1e51c50432aa3
David VanScott <david.vanscott@gmail.com>
2014-04-02 16:30:53 -0400

    Rolling a partial in

--- Scheduler/views/pages/admin/appointments/usersAction.blade.php
+++ Scheduler/views/pages/admin/appointments/usersAction.blade.php
@@ -25,11 +25,11 @@
 	@if (count($schedule) > 0)
 		@foreach ($schedule as $days => $appointments)
 			@if ($days === 0)
-				<h2>Today</h2>
+				<h2>Today <small>{{ Date::now()->format(Config::get('bjga.dates.dateNoDay')) }}</small></h2>
 			@elseif ($days === 1)
-				<h2>Tomorrow</h2>
+				<h2>Tomorrow <small>{{ Date::now()->addDay()->format(Config::get('bjga.dates.dateNoDay')) }}</small></h2>
 			@elseif ($days >= 2 and $days <= 6)
-				<h2>{{ Date::now()->addDays($days)->format(Config::get('bjga.dates.day.long')) }}</h2>
+				<h2>{{ Date::now()->addDays($days)->format(Config::get('bjga.dates.day.long')) }} <small>{{ Date::now()->addDays($days)->format(Config::get('bjga.dates.dateNoDay')) }}</small></h2>
 			@else
 				<h2>{{ Date::now()->addDays($days)->format(Config::get('bjga.dates.date')) }}</h2>
 			@endif
@@ -39,78 +39,7 @@
 				<?php $appt = $a->appointment;?>
 
 				@if ($appt->service->isLesson())
-					<div class="row">
-						<div class="col-sm-3 col-md-2 col-lg-2">
-							<p class="text-sm"><strong>{{ $appt->start->format(Config::get('bjga.dates.time')) }} - {{ $appt->end->format(Config::get('bjga.dates.time')) }}</strong></p>
-						</div>
-						<div class="col-sm-9 col-md-5 col-lg-6">
-							<p class="lead"><strong>{{ trim($appt->service->name) }}</strong></p>
-						</div>
-						<div class="col-sm-12 col-md-5 col-lg-4">
-							<div class="visible-md visible-lg">
-								<div class="btn-toolbar pull-right">
-									@if ($appt->service->isLesson() or $appt->service->isProgram())
-										<div class="btn-group">
-											<a href="#" class="btn btn-sm btn-default icn-size-16 js-email" data-service="{{ $appt->service->id }}" data-appt="{{ $appt->id }}">{{ $_icons['email'] }}</a>
-										</div>
-										
-										<div class="btn-group">
-											@if ($appt->service->isLesson())
-												<a href="{{ URL::route('admin.appointment.edit', array($appt->id)) }}" class="btn btn-sm btn-default icn-size-16">{{ $_icons['edit'] }}</a>
-											@else
-												<a href="{{ URL::route('admin.service.edit', array($appt->service->id)) }}" class="btn btn-sm btn-default icn-size-16">{{ $_icons['edit'] }}</a>
-											@endif
-										</div>
-
-										@if ($appt->service->isLesson())
-											@if ((bool) $appt->userAppointments->first()->paid === false)
-												<div class="btn-group">
-													<a href="#" class="btn btn-sm btn-primary icn-size-16 js-markAsPaid" data-appt="{{ $appt->userAppointments->first()->id }}">{{ $_icons['check'] }}</a>
-												</div>
-											@endif
-										@else
-											<div class="btn-group">
-												<a href="#" class="btn btn-sm btn-default icn-size-16 js-attendees" data-id="{{ $appt->id }}">{{ $_icons['users'] }}</a>
-											</div>
-										@endif
-
-										<div class="btn-group">
-											<a href="#" class="btn btn-sm btn-danger icn-size-16 js-withdraw" data-appointment="{{ $appt->id }}">{{ $_icons['reject'] }}</a>
-										</div>
-									@else
-										<div class="btn-group">
-											<a href="{{ URL::route('admin.staff.block') }}" class="btn btn-sm btn-default icn-size-16">{{ $_icons['calendar'] }}</a>
-										</div>
-									@endif
-								</div>
-							</div>
-							<div class="visible-xs visible-sm">
-								@if ($appt->service->isLesson() or $appt->service->isProgram())
-									<p><a href="#" class="btn btn-lg btn-block btn-default icn-size-16 js-email" data-service="{{ $appt->service->id }}" data-appt="{{ $appt->id }}">{{ $_icons['email'] }}</a></p>
-									
-									<p>
-										@if ($appt->service->isLesson())
-											<a href="{{ URL::route('admin.appointment.edit', array($appt->id)) }}" class="btn btn-lg btn-block btn-default icn-size-16">{{ $_icons['edit'] }}</a>
-										@else
-											<a href="{{ URL::route('admin.service.edit', array($appt->service->id)) }}" class="btn btn-lg btn-block btn-default icn-size-16">{{ $_icons['edit'] }}</a>
-										@endif
-									</p>
-
-									@if ($appt->service->isLesson())
-										@if ((bool) $appt->userAppointments->first()->paid === false)
-											<p><a href="#" class="btn btn-lg btn-block btn-primary icn-size-16 js-markAsPaid" data-appt="{{ $appt->userAppointments->first()->id }}">{{ $_icons['check'] }}</a></p>
-										@endif
-									@else
-										<p><a href="#" class="btn btn-lg btn-block btn-default icn-size-16 js-attendees" data-id="{{ $appt->id }}">{{ $_icons['users'] }}</a></p>
-									@endif
-
-									<p><a href="#" class="btn btn-lg btn-block btn-danger icn-size-16 js-withdraw" data-appointment="{{ $appt->id }}">{{ $_icons['reject'] }}</a></p>
-								@else
-									<p><a href="{{ URL::route('admin.staff.block') }}" class="btn btn-lg btn-block btn-default icn-size-16">{{ $_icons['calendar'] }}</a></p>
-								@endif
-							</div>
-						</div>
-					</div>
+					{{ View::make('partials.appointments.instructorRow')->withAppt($appt) }}
 				@else
 					{{ partial('common/alert', array('class' => ' alert-info', 'content' => "{$user->name} is enrolled in {$appt->service->name} today.")) }}
 				@endif
