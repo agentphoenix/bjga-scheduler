82706672aac2561df57929c3dc2722504deee5e0
David VanScott <david.vanscott@gmail.com>
2014-04-03 17:27:51 -0400

    Or, I may be an idiot...

--- Scheduler/views/pages/ajax/lessonServiceDetails.blade.php
+++ Scheduler/views/pages/ajax/lessonServiceDetails.blade.php
@@ -5,17 +5,7 @@
 		<div class="col-lg-12">
 			<div class="form-group">
 				<label class="control-label">Price</label>
-				<h3 class="text-success price-details">
-					@if ($service->price > 0)
-						@if ($service->occurrences > 1)
-							${{ round(($service->price * $service->occurrences) / ($service->occurrences / 4), 2) }} <small>per month</small>
-						@else
-							${{ $service->price }}
-						@endif
-					@else
-						Free
-					@endif
-				</h3>
+				<h3 class="text-success price-details">{{ $service->present()->price }}</h3>
 			</div>
 		</div>
 	</div>
