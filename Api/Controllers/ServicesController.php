c32967bbf2cf6d0378c3e2e508e7bdf412937924
David VanScott <david.vanscott@gmail.com>
2014-05-13 07:55:27 -0400

    More API work

--- Api/Controllers/ServicesController.php
+++ Api/Controllers/ServicesController.php
@@ -1,15 +1,13 @@
 <?php namespace Scheduler\Api\Controllers;
 
 use ServiceModel;
-use Scheduler\Api\Transformers\ServiceTransformer;
 
 class ServicesController extends ApiController {
 
 	public function index()
 	{
-		$services = ServiceModel::all();
-
-		return $this->respondWithCollection($services, new ServiceTransformer);
+		return ServiceModel::first();
+		return ServiceModel::all();
 	}
 
 	public function showByCategory($category)
@@ -18,17 +16,12 @@ class ServicesController extends ApiController {
 			->orderBy('order', 'asc')
 			->get();
 
-		if ( ! $services)
+		if ($services->count() == 0)
 		{
 			return $this->errorNotFound('No services found');
 		}
 
-		if ($services->count() == 1)
-		{
-			return $this->respondWithItem($services->first(), new ServiceTransformer);
-		}
-
-		return $this->respondWithCollection($services, new ServiceTransformer);
+		return $services;
 	}
 
 	public function showByName($name)
@@ -37,17 +30,12 @@ class ServicesController extends ApiController {
 			->orderBy('order', 'asc')
 			->get();
 
-		if ( ! $services)
+		if ($services->count() == 0)
 		{
-			return $this->errorNotFound('No service found');
+			return $this->errorNotFound('No services found');
 		}
 
-		if ($services->count() == 1)
-		{
-			return $this->respondWithItem($services->first(), new ServiceTransformer);
-		}
-
-		return $this->respondWithCollection($services, new ServiceTransformer);
+		return $services;
 	}
 
 }
