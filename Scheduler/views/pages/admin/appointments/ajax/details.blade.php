b6bf2dceb0b59f89ccd61dad72493aa4d80ca217
David VanScott <david.vanscott@gmail.com>
2014-06-30 09:45:58 -0400

    Work on some fixes for appt details

--- Scheduler/views/pages/admin/appointments/ajax/details.blade.php
+++ Scheduler/views/pages/admin/appointments/ajax/details.blade.php
@@ -33,7 +33,7 @@
 @endif
 
 @if ($appt->service->isLesson() or $appt->service->isProgram())
-	<p><a href="#" class="btn btn-lg btn-block btn-default icn-size-16 js-email" data-service="{{ $appt->service->id }}" data-appt="{{ $appt->id }}">Email Attendees</a></p>
+	<p><a href="#" class="btn btn-lg btn-block btn-default icn-size-16 js-mobile-email" data-service="{{ $appt->service->id }}" data-appt="{{ $appt->id }}">Email Attendees</a></p>
 	
 	<p>
 		@if ($appt->service->isLesson())
@@ -52,12 +52,67 @@
 			<p><a href="#" class="btn btn-lg btn-block btn-primary icn-size-16 js-markAsPaid" data-appt="{{ $appt->userAppointments->first()->id }}">Mark as Paid</a></p>
 		@endif
 	@else
-		<p><a href="#" class="btn btn-lg btn-block btn-default icn-size-16 js-attendees" data-id="{{ $appt->id }}">See All Attendees</a></p>
+		<p><a href="#" class="btn btn-lg btn-block btn-default icn-size-16 js-mobile-attendees" data-id="{{ $appt->id }}">See All Attendees</a></p>
 	@endif
 
 	@if ( ! $appt->hasStarted())
-		<p><a href="#" class="btn btn-lg btn-block btn-danger icn-size-16 js-withdraw" data-type="staff" data-appointment="{{ $appt->id }}">Cancel Appointment</a></p>
+		<p><a href="#" class="btn btn-lg btn-block btn-danger icn-size-16 js-mobile-withdraw" data-type="staff" data-appointment="{{ $appt->id }}">Cancel Appointment</a></p>
 	@endif
 @else
 	<p><a href="{{ URL::route('admin.staff.schedule', array($_currentUser->staff->id)) }}" class="btn btn-lg btn-block btn-default icn-size-16">View Schedule</a></p>
 @endif
+
+{{ View::make('partials.jsMarkAsPaid') }}
+<script>
+
+	$('.js-mobile-attendees').on('click', function(e)
+	{
+		e.preventDefault();
+
+		var id = $(this).data('id');
+
+		$('#apptDetails').modal('hide');
+
+		$('#attendees').modal({
+			remote: "{{ URL::to('admin/appointment/attendees/appointment') }}/" + id
+		}).modal('show');
+	});
+	
+	$('.js-mobile-email').on('click', function(e)
+	{
+		e.preventDefault();
+
+		var service = $(this).data('service');
+		var appt = $(this).data('appt');
+
+		$('#apptDetails').modal('hide');
+
+		$('#sendEmail').modal({
+			remote: "{{ URL::to('ajax/user/email/service') }}/" + service + "/appt/" + appt
+		}).modal('show');
+	});
+
+	$('.js-mobile-withdraw').on('click', function(e)
+	{
+		e.preventDefault();
+
+		var id = $(this).data('appointment');
+		var type = $(this).data('type');
+
+		$('#apptDetails').modal('hide');
+
+		if (type == "staff")
+		{
+			$('#instructorCancel').modal({
+				remote: "{{ URL::to('ajax/cancel/staff') }}/" + id
+			}).modal('show');
+		}
+		else
+		{
+			$('#studentCancel').modal({
+				remote: "{{ URL::to('ajax/cancel/student') }}/" + id
+			}).modal('show');
+		}
+	});
+
+</script>
