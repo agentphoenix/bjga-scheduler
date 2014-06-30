227ab5d9d9088265bb1abeeb73bf0b8bfcf16c48
David VanScott <david.vanscott@gmail.com>
2014-06-29 22:34:57 -0400

    Update the display to show ended appts

--- Scheduler/views/partials/appointments/instructorRowNew.blade.php
+++ Scheduler/views/partials/appointments/instructorRowNew.blade.php
@@ -1,9 +1,9 @@
 <div class="row">
 	<div class="col-xs-4 col-sm-3 col-md-2 col-lg-2">
-		<p class="lead"><strong>{{ $appt->start->format(Config::get('bjga.dates.time')) }}</strong></p>
+		<p class="lead{{ ($appt->hasEnded()) ? ' text-muted' : '' }}"><strong>{{ $appt->start->format(Config::get('bjga.dates.time')) }}</strong></p>
 	</div>
 	<div class="col-xs-8 col-sm-9 col-md-5 col-lg-6">
-		<p class="lead">
+		<p class="lead{{ ($appt->hasEnded()) ? ' text-muted' : '' }}">
 			<a href="#" class="no-color js-details" data-id="{{ $appt->id }}"><strong>
 				@if ($appt->service->isLesson())
 					{{ trim($appt->userAppointments->first()->user->name) }}
