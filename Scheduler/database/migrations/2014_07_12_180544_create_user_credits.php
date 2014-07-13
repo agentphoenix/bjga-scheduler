395d19f1d3abc586c87bb40ac5ab637a82a685f5
David VanScott <david.vanscott@gmail.com>
2014-07-13 12:30:17 -0400

    Add the new claimed field to the migration

--- Scheduler/database/migrations/2014_07_12_180544_create_user_credits.php
+++ Scheduler/database/migrations/2014_07_12_180544_create_user_credits.php
@@ -18,6 +18,7 @@ class CreateUserCredits extends Migration {
 			$table->string('code', 12);
 			$table->string('type');
 			$table->integer('value')->default(0);
+			$table->integer('claimed')->default(0);
 			$table->integer('user_id')->unsigned()->nullable();
 			$table->string('email')->nullable();
 			$table->timestamp('expires')->nullable();
