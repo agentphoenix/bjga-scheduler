c94b5be6e375476677270cc207e69063c5301f39
David VanScott <david.vanscott@gmail.com>
2014-07-13 20:11:34 -0400

    Work on the credits event handler

--- Scheduler/Events/CreditEventHandler.php
+++ Scheduler/Events/CreditEventHandler.php
@@ -1,20 +1,58 @@
 <?php namespace Scheduler\Events;
 
+use Mail,
+	Config;
+
 class CreditEventHandler {
 
 	public function onCreate($credit, $input)
 	{
-		# code...
+		if ( ! empty($credit->user_id) or ! empty($credit->email))
+		{
+			// Compile the basic data
+			$data = [
+				'type'	=> $credit->type,
+				'value'	=> $credit->present()->value,
+			];
+
+			// Compile the user account info
+			if ( ! empty($credit->user_id))
+			{
+				$email = $credit->user->email;
+				$data['subject'] = $subject = "Credit Has Been Added to Your Account";
+				$data['email'] = "user";
+			}
+
+			// Compile the email address info
+			if ( ! empty($credit->email))
+			{
+				$email = $credit->email;
+				$data['subject'] = $subject = "You've Been Given Credit with Brian Jacobs Golf";
+				$data['email'] = "email";
+			}
+
+			// Send the email
+			Mail::queue('emails.creditAdded', $data, function($msg) use ($credit, $email, $subject)
+			{
+				$msg->to($email)
+					->subject(Config::get('bjga.email.subject')." {$subject}")
+					->replyTo(Config::get('bjga.email.contact'));
+			});
+		}
 	}
 
 	public function onDelete($credit)
 	{
-		# code...
+		// Email the user
+
+		// Email the email address
 	}
 
 	public function onUpdate($credit, $input)
 	{
-		# code...
+		// Email the user
+
+		// Email the email address
 	}
 
 }
