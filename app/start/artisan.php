b8b51f78b5cc5e306d9cd865bdee0cac05d21f96
David VanScott <david.vanscott@gmail.com>
2014-07-14 13:16:15 -0400

    Credit cleanup command

--- app/start/artisan.php
+++ app/start/artisan.php
@@ -3,3 +3,4 @@
 Artisan::add(new Scheduler\Commands\ThankYouMessageCommand);
 Artisan::add(new Scheduler\Commands\AppointmentReminderMessageCommand);
 Artisan::add(new Scheduler\Commands\UpdateCalendarCommand);
+Artisan::add(new Scheduler\Commands\CreditCleanupCommand);
