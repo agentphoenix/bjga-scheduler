fc2838b0d819cb29f74e290a3980bff2505be4fc
David VanScott <david.vanscott@gmail.com>
2014-07-13 20:11:49 -0400

    Some RWD work on the credits index

--- Scheduler/views/pages/admin/credits/index.blade.php
+++ Scheduler/views/pages/admin/credits/index.blade.php
@@ -34,26 +34,29 @@
 		<div class="data-table data-table-striped data-table-bordered" id="creditsTable">
 		@foreach ($credits as $c)
 			<div class="row">
-				<div class="col-sm-3">
+				<div class="col-sm-4">
 					<p>
 						<strong class="monospace">{{ $c->code }}</strong>
 						
 						@if ( ! empty($c->user_id))
-							<br><em class="text-muted">{{ $c->present()->user }}</em>
+							<br><span class="text-muted"><span class="tab-icon">{{ $_icons['user'] }}</span><em>{{ $c->present()->user }}</em></span>
 						@endif
 
 						@if ( ! empty($c->email))
-							<br><em class="text-muted">{{ $c->present()->email }}</em>
+							<br><span class="text-muted"><span class="tab-icon tab-icon-down1">{{ $_icons['email'] }}</span><em>{{ $c->present()->email }}</em></span>
 						@endif
 					</p>
 				</div>
-				<div class="col-sm-3">
+				<div class="col-sm-4 col-md-2">
 					<p>{{ $c->present()->value }}</p>
 				</div>
-				<div class="col-sm-3">
+				<div class="col-sm-4 col-md-3">
 					<p>{{ $c->present()->remaining }}</p>
 				</div>
-				<div class="col-sm-6 col-lg-3">
+
+				<div class="clearfix visible-sm-block"></div>
+
+				<div class="col-md-3">
 					<div class="visible-md visible-lg">
 						<div class="btn-toolbar pull-right">
 							<div class="btn-group">
@@ -69,12 +72,12 @@
 					</div>
 					<div class="visible-xs visible-sm">
 						<div class="row">
-							<div class="col-sm-4">
+							<div class="col-sm-6">
 								<p><a href="{{ URL::route('admin.credits.edit', array($c->id)) }}" class="btn btn-block btn-lg btn-default icn-size-16">Edit Credit</a></p>
 							</div>
 							
 							@if ($c->type == 'time')
-								<div class="col-sm-4">
+								<div class="col-sm-6">
 									<p><a href="#" class="btn btn-block btn-lg btn-danger icn-size-16 js-staff-action" data-action="delete" data-id="{{ $c->id }}">Delete Credit</a></p>
 								</div>
 							@endif
