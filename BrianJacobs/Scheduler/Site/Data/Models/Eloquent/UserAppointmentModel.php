8556823d68a5f03e515ecab5702950433ef2b1ae
David VanScott <david.vanscott@gmail.com>
2014-07-16 16:17:30 -0400

    Add received to fillable fields

--- BrianJacobs/Scheduler/Site/Data/Models/Eloquent/UserAppointmentModel.php
+++ BrianJacobs/Scheduler/Site/Data/Models/Eloquent/UserAppointmentModel.php
@@ -10,7 +10,7 @@ class UserAppointmentModel extends Model {
 	protected $table = 'users_appointments';
 
 	protected $fillable = array(
-		'appointment_id', 'recur_id', 'occurrence_id', 'user_id', 'paid', 'amount',
+		'appointment_id', 'recur_id', 'occurrence_id', 'user_id', 'paid', 'amount', 'received',
 	);
 
 	protected $softDelete = true;
