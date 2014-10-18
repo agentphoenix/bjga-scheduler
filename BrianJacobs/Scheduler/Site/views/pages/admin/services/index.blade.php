484fd4421045e096d1d6581314e3b9d276abd076
David VanScott <david.vanscott@gmail.com>
2014-10-18 11:29:10 -0400

    Remove service descriptions

--- BrianJacobs/Scheduler/Site/views/pages/admin/services/index.blade.php
+++ BrianJacobs/Scheduler/Site/views/pages/admin/services/index.blade.php
@@ -52,7 +52,6 @@
 								<span class="label label-warning">Inactive</span>
 							@endif
 						</p>
-						<p class="text-sm text-muted">{{ $service->description }}</p>
 					</div>
 					<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
 						<div class="visible-md visible-lg">
