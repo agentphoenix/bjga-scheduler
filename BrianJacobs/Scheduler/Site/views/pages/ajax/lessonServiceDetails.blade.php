33936a7b1562f61d29d997294faf8d2c8189e113
David VanScott <david.vanscott@gmail.com>
2014-11-08 18:13:53 -0500

    Quick tweak

--- BrianJacobs/Scheduler/Site/views/pages/ajax/lessonServiceDetails.blade.php
+++ BrianJacobs/Scheduler/Site/views/pages/ajax/lessonServiceDetails.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
 	<div class="row">
-		<div class="col-lg-6">
+		<div class="col-sm-6">
 			<div class="form-group">
 				<label class="control-label">Price</label>
 				<h3 class="text-success price-details">{{ $service->present()->price }}</h3>
 			</div>
 		</div>
-		<div class="col-lg-6">
+		<div class="col-sm-6">
 			<div class="form-group">
 				<label class="control-label">Instructor</label>
 				<h3 class="text-success price-details">{{ $service->present()->staffName }}</h3>
