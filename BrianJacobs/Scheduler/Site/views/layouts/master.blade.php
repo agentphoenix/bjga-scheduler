17ea583dbfcfd644bb4c48529aa7597aebeb7a18
David VanScott <david.vanscott@gmail.com>
2014-12-14 09:44:02 -0500

    More Open Graph fixes

--- BrianJacobs/Scheduler/Site/views/layouts/master.blade.php
+++ BrianJacobs/Scheduler/Site/views/layouts/master.blade.php
@@ -7,7 +7,12 @@
 	<meta name="author" content="Brian Jacobs">
 	<meta name="viewport" content="width=device-width">
 	<meta name="application-name" content="Brian Jacobs Golf Scheduler">
+	
 	<meta property="og:image" content="{{ URL::asset('apple-touch-icon.png') }}">
+	<meta property="og:url" content="{{ Request::url() }}">
+	<meta property="og:title" content="@yield('title') &bull; Brian Jacobs Golf">
+	<meta property="og:description" content="Brian Jacobs Golf is committed to providing the best golf instruction in western New York and helping golfers of all skills be more consistent and enjoy the game more.">
+	
 	<link rel="icon" type="image/x-icon" href="{{ URL::asset('favicon.ico?v2') }}">
 	<link rel="apple-touch-icon-precomposed" href="{{ URL::asset('apple-touch-icon.png') }}">
 	<link rel="image_src" href="{{ URL::asset('apple-touch-icon.png') }}">
