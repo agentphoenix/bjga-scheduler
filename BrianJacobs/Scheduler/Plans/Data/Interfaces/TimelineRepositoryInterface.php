bfcabd21ba3fa71876de7f57a55da0a2f346bbcb
David VanScott <david.vanscott@gmail.com>
2015-02-10 16:15:34 -0500

    Remove the timeline files

--- BrianJacobs/Scheduler/Plans/Data/Interfaces/TimelineRepositoryInterface.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Plans\Data\Interfaces;
-
-use Scheduler\Data\Interfaces\BaseRepositoryInterface;
-
-interface TimelineRepositoryInterface extends BaseRepositoryInterface {
-
-	public function create(array $data);
-
-}
--- BrianJacobs/Scheduler/Plans/Data/Presenters/TimelinePresenter.php
+++
@@ -1,6 +0,0 @@
-<?php namespace Plans\Data\Presenters;
-
-use Config, Markdown;
-use Laracasts\Presenter\Presenter;
-
-class TimelinePresenter extends Presenter {}
