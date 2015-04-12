6b60e38936b786e48bfd481f412aefa0aa7b1c79
David VanScott <david.vanscott@gmail.com>
2015-04-12 12:33:09 -0400

    Cleaned up some emails

--- BrianJacobs/Scheduler/Site/views/emails/bookedLesson.blade.php
+++ BrianJacobs/Scheduler/Site/views/emails/bookedLesson.blade.php
@@ -4,10 +4,10 @@
 		<meta charset="utf-8">
 	</head>
 	<body>
-		@if ($recurring)
-			<p>You have successfully booked a {{ $service }} for {{ $date }} from {{ $start }} to {{ $end }}. Make sure to take note of your appointment time and try to arrive 10 minutes before the scheduled start time so we can begin on time.</p>
+		<p>You have successfully booked a {{ $service }} for {{ $date }} from {{ $start }} to {{ $end }} at {{ $location }}. Make sure to take note of your appointment time/location and try to arrive 10 minutes before the scheduled start time so we can begin on time.</p>
 
-			<p>Since this is a recurring service, you will have additional scheduled appointments. The schedule of your appointments and total due for each is below. You can also log in to the Brian Jacobs Golf Scheduler to see your full schedule.</p>
+		@if ($recurring)
+			<p>Since this is a recurring service, you will have additional scheduled appointments. The schedule of your appointments, total due for each, and location are below. You can also log in to the Brian Jacobs Golf Scheduler to see your full schedule.</p>
 
 			<p>A reminder email will be sent 24 hours before each appointment.</p>
 
@@ -17,8 +17,6 @@
 			@endforeach
 			</ul>
 		@else
-			<p>You have successfully booked a {{ $service }} for {{ $date }} from {{ $start }} to {{ $end }} at {{ $location }}. Make sure to take note of your appointment time and try to arrive 10 minutes before the scheduled start time so we can begin on time.</p>
-
 			<p>A reminder email will be sent 24 hours before your appointment.</p>
 		@endif
 
