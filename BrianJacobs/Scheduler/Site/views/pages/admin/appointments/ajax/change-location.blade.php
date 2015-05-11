07a9f5c7a0b9b620ea09a68d4f0becd84ffe9bd7
David VanScott <david.vanscott@gmail.com>
2015-05-11 11:20:45 -0400

    Clarify what the day location change does
    Provide some more context around the fact that changing for a day ONLY
    changes for that day and not for their entire schedule

--- BrianJacobs/Scheduler/Site/views/pages/admin/appointments/ajax/change-location.blade.php
+++ BrianJacobs/Scheduler/Site/views/pages/admin/appointments/ajax/change-location.blade.php
@@ -1,4 +1,6 @@
-<p class="alert alert-warning">Changes to your location for a specific day will only affect private lessons and not programs. If you need to move a program to another location for a single day, please communicate that change to the attendees directly. If you need to move a program to another location for the entirety of the program, edit the program and change its location.</p>
+<p>This will change your scheduled location <strong>for this day only</strong>. If you need to move your entire schedule to another location for this day of the week moving forward, <a href="{{ route('admin.staff.schedule', [$user->staff->id]) }}">edit your regular availability</a> and change your location from there.</p>
+
+<p class="alert alert-warning"><strong>Note:</strong> Changes to your location for a specific day will only affect private lessons and not programs. If you need to move a program to another location for a single day, please communicate that change to the attendees directly. If you need to move a program to another location for the entirety of the program, edit the program and change its location.</p>
 
 {{ Form::open(['route' => 'admin.appointment.changeLocation', 'method' => 'put', 'class' => 'form-horizontal']) }}
 	<div class="form-group">
