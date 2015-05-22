91c1f5f2c62889bdd9b941482c44ea52d80d9143
David VanScott <david.vanscott@gmail.com>
2015-05-22 13:11:37 -0400

    Pull in the changes from some moving

--- BrianJacobs/Scheduler/Plans/Controllers/PlanController.php
+++ BrianJacobs/Scheduler/Plans/Controllers/PlanController.php
@@ -1,9 +1,13 @@
 <?php namespace Plans\Controllers;
 
 use View,
+	Event,
+	Input,
+	Redirect,
 	GoalRepositoryInterface,
 	PlanRepositoryInterface,
-	UserRepositoryInterface;
+	UserRepositoryInterface,
+	StaffRepositoryInterface;
 use Scheduler\Controllers\BaseController;
 
 class PlanController extends BaseController {
@@ -11,15 +15,31 @@ class PlanController extends BaseController {
 	protected $goals;
 	protected $plans;
 	protected $users;
+	protected $staff;
 
 	public function __construct(PlanRepositoryInterface $plans,
-			GoalRepositoryInterface $goals, UserRepositoryInterface $users)
+			GoalRepositoryInterface $goals, UserRepositoryInterface $users,
+			StaffRepositoryInterface $staff)
 	{
 		parent::__construct();
 
 		$this->goals = $goals;
 		$this->plans = $plans;
 		$this->users = $users;
+		$this->staff = $staff;
+	}
+
+	public function index()
+	{
+		// Get all the development plans
+		$plans = ($this->currentUser->access() == 4)
+			? $this->plans->all(['user', 'activeGoals', 'instructors', 'instructors.user'])
+			: $this->plans->getInstructorPlans($this->currentUser->staff);
+
+		// Can we create more plans?
+		$createAllowed = ($plans->count() < 3);
+
+		return View::make('pages.devplans.plans.index', compact('plans', 'createAllowed'));
 	}
 
 	public function show($userId = false)
@@ -58,6 +78,99 @@ class PlanController extends BaseController {
 		}
 
 		return $this->errorNotFound("This user does not have a development plan.");
+	}
+
+	public function create()
+	{
+		// Get all the users
+		$users[''] = "Please select a user";
+		$users += $this->users->withoutDevelopmentPlan();
+
+		return partial('common/modal_content', [
+			'modalHeader'	=> "Add Development Plan",
+			'modalBody'		=> View::make('pages.devplans.plans.create', compact('users')),
+			'modalFooter'	=> false,
+		]);
+	}
+
+	public function store()
+	{
+		// Create the plan
+		$plan = $this->plans->create(Input::all(), $this->currentUser->staff);
+
+		// Fire the event
+		Event::fire('plan.created', [$plan]);
+
+		return Redirect::route('admin.plan.index')
+			->with('messageStatus', 'success')
+			->with('message', "Development plan created!");
+	}
+
+	public function edit($id)
+	{
+		// Get the plan
+		$plan = $this->plans->getById($id, ['user', 'instructors', 'instructors.user']);
+
+		// Get all the instructors
+		$staff[''] = "Please select an instructor";
+		$staff += $this->staff->allForDropdown();
+
+		foreach ($plan->instructors as $instructor)
+		{
+			unset($staff[$instructor->id]);
+		}
+
+		return partial('common/modal_content', [
+			'modalHeader'	=> "Add Instructor to Development Plan",
+			'modalBody'		=> View::make('pages.devplans.plans.instructors', compact('plan', 'staff')),
+			'modalFooter'	=> false,
+		]);
+	}
+
+	public function update($id)
+	{
+		// Update the plan
+		$plan = $this->plans->addInstructor($id, Input::get('instructor'));
+
+		// Fire the event
+		Event::fire('plan.updated', [$plan, Input::get('instructor')]);
+
+		return Redirect::route('admin.plan.index')
+			->with('messageStatus', 'success')
+			->with('message', "Instructor added to development plan!");
+	}
+
+	public function remove($id)
+	{
+		// Get the plan
+		$plan = $this->plans->getById($id, ['user']);
+
+		return partial('common/modal_content', [
+			'modalHeader'	=> "Remove Development Plan",
+			'modalBody'		=> View::make('pages.devplans.plans.remove', compact('plan')),
+			'modalFooter'	=> false,
+		]);
+	}
+
+	public function destroy($id)
+	{
+		// Remove the plan
+		$plan = $this->plans->delete($id);
+
+		// Fire the event
+		Event::fire('plan.deleted', [$plan]);
+
+		return Redirect::route('admin.plan.index')
+			->with('messageStatus', 'success')
+			->with('message', "Development plan removed!");
+	}
+
+	public function removeInstructor()
+	{
+		// Remove the instructor
+		$this->plans->removeInstructor(Input::get('plan'), Input::get('instructor'));
+
+		return json_encode(['code' => 1]);
 	}
 
 	public function goal($userId = false, $goalId)
