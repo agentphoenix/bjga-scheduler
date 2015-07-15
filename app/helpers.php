70e09433902cbb2041c26eaa8eb354b4c6e457a5
David VanScott <david.vanscott@gmail.com>
2015-07-15 16:18:15 -0400

    Add ordinal helper

--- app/helpers.php
+++ app/helpers.php
@@ -85,3 +85,15 @@ if ( ! function_exists('event'))
 		return app('events')->fire($event, $payload, $halt);
 	}
 }
+
+if ( ! function_exists('ordinal'))
+{
+	function ordinal($number)
+	{
+		$ends = ['th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th'];
+
+		if ((($number % 100) >= 11) and (($number % 100) <= 13)) return $number.'th';
+
+		return $number.$ends[$number % 10];
+	}
+}
